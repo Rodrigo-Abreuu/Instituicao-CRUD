925459e82c8f5a96816625825737b25e6f65f1df
Rodrigo Abreu <rodrigomourabreu@gmail.com>
2021-08-17 15:00:33 -0300

    Criando páginas de Instituição

---
+++ Controllers/InstituicaoController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Projeto.Models;
+
+namespace Projeto.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao(){
+                InstituicaoID = 1,
+                Nome = "IFCE",
+                Endereco = "Ceará"
+            },
+
+            new Instituicao(){
+                InstituicaoID = 2,
+                Nome = "UFC",
+                Endereco = "Ceará"
+            },
+
+            new Instituicao(){
+                InstituicaoID = 3,
+                Nome = "UECE",
+                Endereco = "Ceará"
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(instituicoes);
+        }
+
+        //Get: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Instituicao instituicao)
+        {
+            instituicao.InstituicaoID = instituicoes.Select(i => i.InstituicaoID).Max() + 1;
+            instituicoes.Add(instituicao);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
